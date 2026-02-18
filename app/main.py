36d3c0c2eb897300e461b4cdb145844a132bd9ca
Mohd jawwad <mohdjawwad73@gmail.com>
2026-02-18 20:41:15 +0530

    CRUD SQLALCHEMY

--- app/main.py
+++ app/main.py
@@ -1,23 +1,26 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from typing import Optional
 from pydantic import BaseModel,Field
-from . import models
-from .database import engine, get_db
+from . import models, schema
+from .database import SessionLocal, engine
 import psycopg
 from psycopg import rows
 from sqlalchemy.orm import Session
 import time
+from sqlalchemy import select
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-class Post(BaseModel):
-    title:str = Field(min_length=5, max_length=50)
-    content:str = Field(min_length=10)
-    published: bool = True
 
 while True:
     try:
@@ -53,54 +56,87 @@ def root():
     return {"message":"Hello World"}
 
 @app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
+def test_posts():
+
     return {"status": "success"}
+
+
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts=cursor.fetchall()
-    print(posts)
+def get_posts(db: Session = Depends(get_db)):
+    #cursor.execute("""SELECT * FROM posts""")
+    #posts=cursor.fetchall()
+    #print(posts)
+    stmt=select(models.Post)
+    posts=db.execute(stmt).scalars().all()
     return {"data":posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 #prevent duplicate titles
-def create_posts(post: Post, ):
+def create_posts(post: schema.PostCreate, db: Session = Depends(get_db)):
     #run a select query to chcek if title exists.if exists raise http
     
-    cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s)
-                   RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post=cursor.fetchone()
-    conn.commit()
-    return {"data":new_post}
+    # cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s)
+    #                RETURNING * """,
+    #                (post.title, post.content, post.published))
+    # new_post=cursor.fetchone()
+    # conn.commit()
+    # return {"data":new_post}
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
+
+
 @app.get("/posts/{id}")
 # ineed to modify my post so that
-def get_post(id:int, response: Response):
-    cursor.execute("""SELECT * FROM posts where id=%s""",(id,))
-    np=cursor.fetchone()
-    if not np:
-        response.status_code= status.HTTP_404_NOT_FOUND
-        return {"message":f"post with id {id} was not found"}
-    return {"post_detail":np}
+def get_post(id:int, response: Response, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts where id=%s""",(id,))
+    # np=cursor.fetchone()
+    # if not np:
+    #     response.status_code= status.HTTP_404_NOT_FOUND
+    #     return {"message":f"post with id {id} was not found"}
+    # return {"post_detail":np}
+    stmt=select(models.Post).where(models.Post.id==id)
+    result=db.execute(stmt).scalar_one_or_none()
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    return {"post_detail": result}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
-    cursor.execute(""" DELETE FROM posts WHERE id=%s returning *""",(id,))
-    delete_post=cursor.fetchone()
-    conn.commit()
+def delete_post(id:int, db: Session = Depends(get_db)):
+    # cursor.execute(""" DELETE FROM posts WHERE id=%s returning *""",(id,))
+    # delete_post=cursor.fetchone()
+    # conn.commit()
 
+    post = db.get(models.Post, id)
 
-    if delete_post == None:
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with this id does not exits")
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+    db.delete(post)
+    db.commit() 
+
+    return {"message": "Post deleted successfully"}
 
 @app.put("/posts/{id}")
-def update_post(id:int, post:Post):
-    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s
-                   WHERE id=%s returning * """,(post.title, post.content, post.published, id,))
-    index=cursor.fetchone()
-    conn.commit()
-    if index==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+def update_post(id:int, updated_post:schema.PostCreate, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s
+    #                WHERE id=%s returning * """,(post.title, post.content, post.published, id,))
+    # index=cursor.fetchone()
+    # conn.commit()
+    post = db.get(models.Post, id)
 
-    return {"data":index}
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    
+    post.title = updated_post.title
+    post.content = updated_post.content
+
+    db.commit()
+    db.refresh(post)
+
+
+    return {"data":post}
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import TIMESTAMP, text
+from datetime import datetime
 from .database import Base
 
 class Post(Base):
@@ -9,3 +11,4 @@ class Post(Base):
     title: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(String, nullable=False)
     published: Mapped[bool] = mapped_column(server_default='True', nullable=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
